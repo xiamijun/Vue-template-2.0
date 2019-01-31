c7e39ee5f5fbab9e6b01ccd9586a64d5b9dbd2f5
xiamijun <171571466@qq.com>
2019-01-31 13:49:09 +0800

    init

---
+++ src/router/modules/module-one/index.js
@@ -0,0 +1,13 @@
+/** @format */
+import Login from '@/views/login';
+export const routerMap = [
+  {
+    path: '/',
+    name: 'login',
+    meta: {
+      title: '登录',
+      by: 'xiamijun'
+    },
+    component: Login
+  }
+];
